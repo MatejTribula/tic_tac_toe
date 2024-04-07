7f5d57c765bbe44f248499505e4e3a918cb4f1ee
mattveym <matej.tribula@gmail.com>
2024-04-07 21:00:40 +0200

    added all the files (HTML, CSS, js) and created description in the script.js file

---
+++ script.js
@@ -0,0 +1,11 @@
+// PROJECT
+// 1. gameboard object
+// 2. the array will be inside of a Gameboard object
+// 3. create objects for players
+// 4.logic of the game
+//set names for players and by blicking button game starts
+//gameboard appears and tells player one that its his turn
+//once one player clicks on one square it is not clickable anymore
+//if one of the patterns is matched the game ends
+//if it doesnt happen after clicking 9 squares it is game over and draw
+// after game is over text is show and button to reset the game is in
